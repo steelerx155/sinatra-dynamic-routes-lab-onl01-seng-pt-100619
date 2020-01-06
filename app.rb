4a2b2f1c775972e54e3242921f069369e113a47a
Randy Steele <steelerx155@gmail.com>
2020-01-06 15:29:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,4 +24,9 @@ end
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
+  
+  get "/:operation/:number1/:number2"
+   @sum = params[:number1 + :number2]
+ end
+  
 end
